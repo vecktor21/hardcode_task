76ea60d47cfc84a206b2de2e8f4e65b02900810c
Denis Odnourov <denis.ondourov@samgau.com>
2024-02-15 01:41:59 +0600

    create base rest api for item and category controllers

---
+++ TestTask.Gateway/Dtos/CategoryInfoDto.cs
@@ -0,0 +1,17 @@
+﻿namespace TestTask.Gateway.Dtos
+{
+    public class CategoryInfoDto
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public List<CategoryAttributeInfo> AttributesInfo { get; set; }
+    }
+
+    public class CategoryAttributeInfo
+    {
+        public int Id { get; set; }
+        public string AttributeName { get; set; }
+        public int AttributeDataTypeId { get; set; }
+        public string AttributeDataTypeName { get; set;}
+    }
+}
